0f5bda65c616e0fa80bed77a343428adafc6d36e
Luis Cipriani <cunhaluisg5@gmail.com>
2019-12-26 14:29:57 -0300

    Criado o componente Agenda

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
+console.disableYellowBox = true;
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Agenda from './src/screens/Agenda';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = prop => (
+  <Agenda/>
+);
+export default App;
