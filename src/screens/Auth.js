3528ba5caaa177415e72094ea4716887e2b85764
Luis Cipriani <cunhaluisg5@gmail.com>
2020-01-03 16:28:35 -0300

    Criado Input de Autenticação

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -8,7 +8,7 @@ import {
     TouchableOpacity,
     Alert
 } from 'react-native';
-import commonStyles from '../commonStyles';
+import AuthInput from '../components/AuthInput';
 import backgroundImage from '../../assets/imgs/login.jpg';
 
 export default class Auth extends Component {
@@ -39,17 +39,19 @@ export default class Auth extends Component {
                             'Crie a sua conta' : 'Informe seus dados'}
                     </Text>
                     {this.state.stageNew &&
-                        <TextInput placeholder='Nome' style={styles.input}
+                        <AuthInput icon='user' placeholder='Nome' style={styles.input}
                             value={this.state.name}
                             onChangeText={name => this.setState({ name })}/>}
-                    <TextInput placeholder='E-mail' style={styles.input}
+                    <AuthInput icon='at' placeholder='E-mail' style={styles.input}
                         value={this.state.email}
                         onChangeText={email => this.setState({ email })} />
-                    <TextInput placeholder='Senha' style={styles.input}
+                    <AuthInput icon='lock' secureTextEntry={true} 
+                        placeholder='Senha' style={styles.input}
                         value={this.state.password}
                         onChangeText={password => this.setState({ password })}/>
                     {this.state.stageNew &&
-                        <TextInput placeholder='Confirmação'
+                        <AuthInput icon='asterisk' secureTextEntry={true} 
+                            placeholder='Confirmação'
                             style={styles.input} value={this.state.confirmPassword}
                             onChangeText={confirmPassword => this.setState({ confirmPassword })}/>}
                     <TouchableOpacity onPress={this.signinOrSignup}>
